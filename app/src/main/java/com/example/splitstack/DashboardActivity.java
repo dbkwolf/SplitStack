ad16bdf87671c9ff1980d8f4554276958f144779
John <jdaley31415@gmail.com>
2019-05-15 15:49:59 +0200

    Added navi from the dashboard to the event list by clicking on the active events text view

--- app/src/main/java/com/example/splitstack/DashboardActivity.java
+++ app/src/main/java/com/example/splitstack/DashboardActivity.java
@@ -1,10 +1,12 @@
 package com.example.splitstack;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.TextView;
 import com.example.splitstack.Adapter.ActiveEventsAdapter;
 import com.example.splitstack.Adapter.ExpenseAdapter;
@@ -50,4 +52,8 @@ private void setUserID(){
     System.out.println(userIdFromPreviousScreen);
     decentralizedIdForUser = getIntent().getStringExtra("decentralizedIdForUser");
     }
+public void goToEventList(View view){
+        Intent intent =new  Intent(this,EventListActivity.class);
+        startActivity(intent);
+}
 }
