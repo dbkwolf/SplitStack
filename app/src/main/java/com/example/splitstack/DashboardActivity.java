2bde6c73e372b3c0d8f185c6da2ec6c0947210ba
John <jdaley31415@gmail.com>
2019-05-15 14:30:23 +0200

    Added a textview to display the current logged in user for testing the passing of information from the sign in screen to the dash board. I also created a variable that contains the users decentralized id for future database queries to the dashboard controller.

--- app/src/main/java/com/example/splitstack/DashboardActivity.java
+++ app/src/main/java/com/example/splitstack/DashboardActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 import com.example.splitstack.Adapter.ActiveEventsAdapter;
 import com.example.splitstack.Adapter.ExpenseAdapter;
 import com.example.splitstack.Adapter.ParticipantAdapter;
@@ -15,7 +16,7 @@ public class DashboardActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManage;
-
+    private String decentralizedIdForUser="";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,5 +41,13 @@ public class DashboardActivity extends AppCompatActivity {
 
         mRecyclerView.setLayoutManager(mLayoutManage);
         mRecyclerView.setAdapter(mAdapter);
+        setUserID();
+    }
+private void setUserID(){
+   String userIdFromPreviousScreen = getIntent().getStringExtra("userId");
+    TextView userIdText = findViewById(R.id.UserIdText);
+    userIdText.setText("Logged in as " + userIdFromPreviousScreen);
+    System.out.println(userIdFromPreviousScreen);
+    decentralizedIdForUser = getIntent().getStringExtra("decentralizedIdForUser");
     }
 }
